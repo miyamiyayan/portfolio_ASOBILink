fe7ed981d58eb5e95ede3192e20ee471d8a99482
miyamiyayan <dmkm.25.active@gmail.com>
2021-12-31 01:46:54 +0000

    [update]参加希望の非同期通信,next通知機能

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -4,14 +4,12 @@ class JoinsController < ApplicationController
     game = Game.find(params[:asobi_id])
     join = current_member.joins.new(game_id: game.id)
     join.save
-    redirect_to asobi_path(game)
   end
 
   def destroy
     game = Game.find(params[:asobi_id])
     join = current_member.joins.find_by(game_id: game.id)
     join.destroy
-    redirect_to asobi_path(game)
   end
 
 end
