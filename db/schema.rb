08a145cb3d11fe21605e05f6a97b8078d1c864b5
miyamiyayan <dmkm.25.active@gmail.com>
2022-01-03 13:19:52 +0000

    [make]通知用db,next各アソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_141048) do
+ActiveRecord::Schema.define(version: 2022_01_03_131709) do
 
   create_table "games", force: :cascade do |t|
     t.integer "member_id", null: false
@@ -47,6 +47,17 @@ ActiveRecord::Schema.define(version: 2021_12_21_141048) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "visiter_id"
+    t.integer "visited_id"
+    t.integer "game_id"
+    t.integer "comment_id"
+    t.string "action"
+    t.boolean "checked", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "post_comments", force: :cascade do |t|
